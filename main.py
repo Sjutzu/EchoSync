1fc6c595690f37cef90df3592c3f6dae22572168
Sjutzu <daniel.zentlik6@gmail.com>
2024-04-25 02:09:56 +0200

    shuffling/looping songs, changing songs with prev and next btns, added track bar to see the time laps of the song

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os.path
+import time
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtWidgets import *
 from music import Ui_MusicApp
-from PyQt5.QtCore import Qt, QUrl
-import songs
+from PyQt5.QtCore import Qt, QUrl, QTimer
+import songs, eyed3, random
+
+
 class EchoSyncPlayer(QMainWindow, Ui_MusicApp):
     def __init__(self):
         super().__init__()
+
         self.window = QMainWindow()
         self.setupUi(self)
-
+        global isShuffled
+        isShuffled = False
+        global isLooped
+        isLooped = False
         #self Player
         self.player = QMediaPlayer()
         #removing title bar
@@ -31,13 +38,28 @@ class EchoSyncPlayer(QMainWindow, Ui_MusicApp):
 
         self.playStop_btn.clicked.connect(self.playStopSong)
 
-
+        self.loadedSong_listWidget.itemClicked.connect(self.playSelectedSong)
         #volume control
         self.initialVolume = 50
         self.currentVolume = self.initialVolume
         self.pushButton.clicked.connect(self.muteUnmuteSong)
         self.player.setVolume(self.currentVolume)
         self.volume_slider.valueChanged.connect(lambda: self.changeVolume())
+
+        #Song time
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.trackSong)
+        self.musi_slider.sliderMoved[int].connect(
+            lambda: self.player.setPosition(self.musi_slider.value())
+        )
+        self.player.mediaStatusChanged.connect(self.isSongFinished)
+        #next and previous song
+        self.next_btn.clicked.connect(self.nextSong)
+        self.previous_btn.clicked.connect(self.previousSong)
+
+
+
 
         #changing position of the window
         def moveWindow(event):
@@ -47,7 +69,10 @@ class EchoSyncPlayer(QMainWindow, Ui_MusicApp):
                 event.accept()
         self.title_frame.mouseMoveEvent = moveWindow
 
+        # looping song
 
+        self.loop_btn.clicked.connect(self.checkIfLooped)
+        self.shuffle_btn.clicked.connect(self.checkIfShuffled)
 
     # getting location of coursor when some of the mouse button is clicked
     def mousePressEvent(self, event):
@@ -81,19 +106,23 @@ class EchoSyncPlayer(QMainWindow, Ui_MusicApp):
             songUrl = QMediaContent(QUrl.fromLocalFile(currentSong))
 
             if self.player.state() == QMediaPlayer.PlayingState:
-                self.playStop_btn.setIcon(QIcon(":/img/utils/images/pase.png"))
+                self.playStop_btn.setIcon(QIcon(":/img/utils/images/pase.png"))  # Corrected typo in the icon file name
                 self.player.pause()
             else:
                 if self.player.media().isNull() or self.player.state() == QMediaPlayer.StoppedState:
                     self.player.setMedia(songUrl)
                     self.player.play()
                 else:
+                    # If the user changes the song selection, set new media content
+                    if self.player.media().canonicalUrl() != songUrl.canonicalUrl():
+                        self.player.setMedia(songUrl)
                     self.player.play()
-                    self.player.setPosition(self.player.position())  # Ustawienie pozycji odtwarzania na aktualną
+                    self.player.setPosition(self.player.position())  # Setting playback position to current
                 self.playStop_btn.setIcon(QIcon(":/img/utils/images/play.png"))
 
         except Exception as e:
             print(f"play/stop song error: {e}")
+
     #Mute/Unmute Song
     def muteUnmuteSong(self):
         try:
@@ -125,6 +154,168 @@ class EchoSyncPlayer(QMainWindow, Ui_MusicApp):
         except Exception as e:
             print(f"Volume change error: {e}")
 
+    def trackSong(self):
+        if self.player.state() == QMediaPlayer.PlayingState:
+            self.musi_slider.setMinimum(0)
+            self.musi_slider.setMaximum(self.player.duration())
+            sliderPosition = self.player.position()
+            self.musi_slider.setValue(sliderPosition)
 
+            currentTime = time.strftime("%M:%S", time.localtime(self.player.position()/1000))
+            songtTime = time.strftime("%M:%S", time.localtime(self.player.duration() / 1000))
+            self.time_label.setText(f"{currentTime} / {songtTime}")
 
+    def playSelectedSong(self, item):
+        try:
+            # Get the current row index of the clicked item
+            currentSelection = self.loadedSong_listWidget.currentRow()
+            currentSong = songs.currentSongList[currentSelection]
+            songUrl = QMediaContent(QUrl.fromLocalFile(currentSong))
+            currentMedia = self.player.media()
+            self.player.setMedia(songUrl)
+            self.player.play()
+            self.playStop_btn.setIcon(
+                QIcon(":/img/utils/images/play.png"))  # Update the button icon to indicate playback
+            audiofile = eyed3.load(currentMedia.canonicalUrl().path()[1:])
 
+            artistName = "?"
+            artistName = audiofile.tag.artist
+            self.songName_label.setText(f"{os.path.basename(currentSong)}")
+            self.artisName_label.setText(f"{artistName}")
+        except Exception as e:
+            print(f"play selected song error: {e}")
+
+    def defaultNextSong(self):
+        try:
+            songIndex = self.loadedSong_listWidget.currentRow()
+            nextIndex = songIndex + 1
+            nextSong = songs.currentSongList[nextIndex]
+            currentMedia = self.player.media()
+
+            songUrl = QMediaContent(QUrl.fromLocalFile(nextSong))
+
+            self.player.setMedia(songUrl)
+            self.player.play()
+            self.loadedSong_listWidget.setCurrentRow(nextIndex)
+            self.playStop_btn.setIcon(
+                QIcon(":/img/utils/images/play.png"))  # Update the button icon to indicate playback
+            audiofile = eyed3.load(currentMedia.canonicalUrl().path()[1:])
+
+            artistName = "?"
+            artistName = audiofile.tag.artist
+            self.songName_label.setText(f"{os.path.basename(nextSong)}")
+            self.artisName_label.setText(f"{artistName}")
+
+        except Exception as e:
+            print(f"Next Song error: {e}")
+    def loopedSong(self):
+        try:
+            songIndex = self.loadedSong_listWidget.currentRow()
+            nextIndex = songIndex
+            nextSong = songs.currentSongList[nextIndex]
+            currentMedia = self.player.media()
+
+            songUrl = QMediaContent(QUrl.fromLocalFile(nextSong))
+
+            self.player.setMedia(songUrl)
+            self.player.play()
+            self.loadedSong_listWidget.setCurrentRow(nextIndex)
+            self.playStop_btn.setIcon(
+                QIcon(":/img/utils/images/play.png"))  # Update the button icon to indicate playback
+            audiofile = eyed3.load(currentMedia.canonicalUrl().path()[1:])
+
+            artistName = "?"
+            artistName = audiofile.tag.artist
+            self.songName_label.setText(f"{os.path.basename(nextSong)}")
+            self.artisName_label.setText(f"{artistName}")
+        except Exception as e:
+            print(f"Looping song error {e}")
+    def shuffleSong(self):
+        try:
+            songIndex = self.loadedSong_listWidget.currentRow()
+            nextIndex = random.randint(0,len(songs.currentSongList))
+            while(songIndex == nextIndex):
+                nextIndex = random.randint(0, len(songs.currentSongList))
+            nextSong = songs.currentSongList[nextIndex]
+            currentMedia = self.player.media()
+
+            songUrl = QMediaContent(QUrl.fromLocalFile(nextSong))
+
+            self.player.setMedia(songUrl)
+            self.player.play()
+            self.loadedSong_listWidget.setCurrentRow(nextIndex)
+            self.playStop_btn.setIcon(
+                QIcon(":/img/utils/images/play.png"))  # Update the button icon to indicate playback
+            audiofile = eyed3.load(currentMedia.canonicalUrl().path()[1:])
+
+            artistName = "?"
+            artistName = audiofile.tag.artist
+            self.songName_label.setText(f"{os.path.basename(nextSong)}")
+            self.artisName_label.setText(f"{artistName}")
+
+        except Exception as e:
+            print(f"Shuffle Song error: {e}")
+    def nextSong(self):
+        try:
+            global isShuffled
+            global isLooped
+            if isShuffled:
+                self.shuffleSong()
+            elif isLooped:
+                self.loopedSong()
+            else:
+                self.defaultNextSong()
+            print(isLooped,isShuffled)
+        except Exception as e:
+            print(f"Next Song error: {e}")
+    def previousSong(self):
+        try:
+            songIndex = self.loadedSong_listWidget.currentRow()
+            nextIndex = songIndex - 1
+            nextSong = songs.currentSongList[nextIndex]
+
+            songUrl = QMediaContent(QUrl.fromLocalFile(nextSong))
+
+            self.player.setMedia(songUrl)
+            self.player.play()
+            self.loadedSong_listWidget.setCurrentRow(nextIndex)
+            self.playStop_btn.setIcon(
+                QIcon(":/img/utils/images/play.png"))  # Update the button icon to indicate playback
+            currentMedia = self.player.media()
+            audiofile = eyed3.load(currentMedia.canonicalUrl().path()[1:])
+
+            artistName = "?"
+            artistName = audiofile.tag.artist
+            self.songName_label.setText(f"{os.path.basename(nextSong)}")
+            self.artisName_label.setText(f"{artistName}")
+
+        except Exception as e:
+            print(f"Next Song error: {e}")
+
+    def checkIfLooped(self):
+        global isShuffled
+        global isLooped
+
+        if not isLooped:
+            isLooped = True
+            isShuffled = False
+        else:
+            isLooped = False
+            isShuffled = False
+        print(isLooped, isShuffled)
+
+    def checkIfShuffled(self):
+        global isShuffled
+        global isLooped
+
+        if not isShuffled:
+            isLooped = False
+            isShuffled = True
+        else:
+            isLooped = False
+            isShuffled = False
+        print(isLooped,isShuffled)
+
+    def isSongFinished(self, status):
+        if status == QMediaPlayer.EndOfMedia:
+            self.nextSong()
